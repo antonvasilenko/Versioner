08e6fa36817e9d2e0e766701cc7444fb81a0ea6f
Anton Vasilenko <toxa07@gmail.com>
2015-06-17 23:24:00 +0300

    command line params analyzer improved

--- Sources/Program.cs
+++ Sources/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using JetBrains.Annotations;
 using NDesk.Options;
 using System;
@@ -8,193 +9,230 @@ using System.Text;
 
 namespace Versioner
 {
-	public class UserInput
-	{
-		public enum WorkMode { Undefined, Read, Update }
+    public class UserInput
+    {
+        public string FilePath { get; set; }
 
-		public string FilePath { get; set; }
+        public string VersionMask { get; set; }
 
-		public string VersionMask { get; set; }
+        public string Mode { get; set; }
 
-		public WorkMode Mode { get; set; }
+        public string Verbosity { get; set; }
 
-		public Verbosity OutputLevel { get; set; }
+        public bool ShowHelp { get; set; }
+    }
 
-		public bool ShowHelp { get; set; }
-	}
+    public enum Verbosity { Data = 0, Normal = 1, Detailed  = 2 }
 
-	public enum Verbosity { Quiet = 1, Normal = 2, Detailed  = 3 }
+    class Program
+    {
+        private static string[] _verbosities = { "data", "normal", "detailed" };
+        private static string[] _modes = { "read", "update" };
+        private static UserInput _userInput;
 
-	class Program
-	{
-		private static UserInput _userInput;
-		public static Verbosity LogLevel;
+        private static Verbosity _verbosity;
+        private static bool _readMode;
+        private static uint?[] _versionParts;
 
-		static void Main(string[] args)
-		{
-			LogLevel = Verbosity.Normal;
 
-			
+        static void Main(string[] args)
+        {
+            Lo.Verbosity = Verbosity.Normal;
 
-			OptionSet options;
-			_userInput = ParseStartupOptions(args, out options);
-			var analyzeRes = AnalyzeStartupOptions(_userInput);
+            OptionSet options;
+            _userInput = ParseStartupOptions(args, out options);
+            var analyzeRes = AnalyzeStartupOptions(_userInput);
 
-			if (analyzeRes)
-			{
-				LogLevel = _userInput.OutputLevel;
-			}
-			Console.WriteLine("Versioner");
-			Console.WriteLine("Copyright antonv 2015");
+            if (analyzeRes)
+            {
+                Lo.Verbosity = _verbosity;
+            }
+            Lo.Log("Versioner\n");
+            Lo.Log("Copyright antonv 2015\n");
 
-			if (!analyzeRes || _userInput.ShowHelp)
-			{
-				Console.WriteLine();
-				Console.WriteLine("Usage: Versioner --file:\"C:\\work\\AssemblyInfo.cs\"  --read --quiet");
-				Console.WriteLine("Usage: Versioner --file:\"C:\\work\\info.plist\"  --update --normal");
-				Console.WriteLine();
-				options.WriteOptionDescriptions(Console.Out);
-				return;
-			}
+            if (!analyzeRes || _userInput.ShowHelp)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: Versioner --file:\"C:\\work\\AssemblyInfo.cs\"  --read --quiet");
+                Console.WriteLine("Usage: Versioner --file:\"C:\\work\\info.plist\"  --update --normal");
+                Console.WriteLine();
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
 
-			Console.WriteLine("Searching strings in project(s) folders");
-		}
+            Console.WriteLine("Searching strings in project(s) folders");
+        }
 
-		private static OptionSet PrepareOptionsParser(UserInput inputToFill)
-		{
-			var opts = inputToFill;
-			var lParser = new OptionSet
+        private static T GetAttribute<T>() where T : Attribute
+        {
+            return typeof(Program).GetTypeInfo()
+                .Assembly.GetCustomAttribute<T>();
+        }
+
+        private static OptionSet PrepareOptionsParser(UserInput inputToFill)
+        {
+            var opts = inputToFill;
+            var lParser = new OptionSet
 			{
                     {"f|file=", "path to file that contains version (.cs .manifest.xml info.plist)", filePath => opts.FilePath = filePath},
-                    {"v|version=", "version mask to update file with, i.e. 1.0.3.4, *.*.*.1342", v => opts.VersionMask = v},
-                    {"r|read", "use to read out version from file", r => {
-						                                                     if (r != null)
-																			     opts.Mode = UserInput.WorkMode.Read;
-																		 }},
-					{"u|update", "use to update version in file", r => {
-						                                                     if (r != null)
-																			     opts.Mode = UserInput.WorkMode.Update;
-																		 }},
-					{"q|quiet", "set output level to return no log messages", flag => 
-					                                                     {
-						                                                     if (flag != null)
-																			     opts.OutputLevel = Verbosity.Quiet;
-																		 }},
-		            {"n|normal", "set output level to return useful log messages", flag => 
-					                                                     {
-						                                                     if (flag != null)
-																				 opts.OutputLevel = Verbosity.Normal;
-																		 }},
-			        {"d|detailed", "set output level to return all log messages", flag => 
-					                                                     {
-						                                                     if (flag != null)
-																				 opts.OutputLevel = Verbosity.Detailed;
-																		 }},
+                    {"ver|version=", "version mask to update file with, i.e. 1.0.3.4, *.*.*.1342", v => opts.VersionMask = v},
+                    {"m|mode=", string.Format("set the work mode for versioner ({0})", string.Join(" | ", _modes)), m => opts.Mode = m},
+					{"v|verbosity=", string.Format("set output verbosity level ({0})", string.Join(" | ", _verbosities)), v => opts.Verbosity = v}, 
                     {"h|?|help", "shows this help message and exit", h=> opts.ShowHelp = h != null}
                 };
-			return lParser;
-		}
+            return lParser;
+        }
 
-		private static UserInput ParseStartupOptions(IEnumerable<string> args, out OptionSet options)
-		{
-			var userInput = new UserInput
-			{
-				OutputLevel = Verbosity.Normal,
-				ShowHelp = false,
-			};
+        private static UserInput ParseStartupOptions(IEnumerable<string> args, out OptionSet options)
+        {
+            var userInput = new UserInput
+            {
+                Mode = "read",
+                Verbosity = "normal",
+                ShowHelp = false,
+            };
 
-			options = PrepareOptionsParser(userInput);
-			try
-			{
-				options.Parse(args);
-			}
-			catch (OptionException e)
-			{
-				Console.WriteLine(e.Message);
-				userInput.ShowHelp = true;
-			}
-			return userInput;
-		}
+            options = PrepareOptionsParser(userInput);
+            try
+            {
+                options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine(e.Message);
+                userInput.ShowHelp = true;
+            }
+            return userInput;
+        }
 
-		private static bool AnalyzeStartupOptions(UserInput input)
-		{
-			if (input.ShowHelp)
-				return true;
+        private static bool AnalyzeStartupOptions(UserInput input)
+        {
+            if (input.ShowHelp)
+                return true;
 
-			if (input.OutputLevel != Verbosity.Quiet)
-			{
-				Console.WriteLine("Output level set to {0}. Consider using quiet in pipelines to return result only", input.OutputLevel);
-			}
+            if (String.IsNullOrEmpty(input.FilePath))
+            {
+                Console.WriteLine("path to file with version not provided, use '-f' or '--file' option");
+                return false;
+            }
+            if (!File.Exists(input.FilePath))
+            {
+                Console.WriteLine("File '{0}' doesn't exists. Check path in '-f' or '--files", input.FilePath);
+                return false;
+            }
 
-			if (String.IsNullOrEmpty(input.FilePath))
-			{
-				Console.WriteLine("path to file with version not provided, use '-f' or '--file' option");
-				return false;
-			}
-			if (!File.Exists(input.FilePath))
-			{
-				Console.WriteLine("File '{0}' doesn't exists. Check path in '-f' or '--files", input.FilePath);
-				return false;
-			}
+            if (String.IsNullOrWhiteSpace(input.Verbosity))
+            {
+                Console.WriteLine("Output verbosity level not set.");
+                return false;
+            }
 
-			if (input.Mode == UserInput.WorkMode.Undefined)
-			{
-				Console.WriteLine("Mode not provided, use '-r|-read' to read or '-u|-update' to update version");
-				return false;
-			}
+            var pos = Array.IndexOf(_verbosities, input.Verbosity.ToLowerInvariant());
+            if (pos < 0)
+            {
+                Console.WriteLine("Unexpected output verbosity value. Allowed values are '{0}')", string.Join(", ", _verbosities));
+                return false;
+            }
+            _verbosity = (Verbosity)pos;
+            if (_verbosity != Verbosity.Data)
+            {
+                Lo.Log("For using versioner in pipelines consider use verbosity level '{0}' to return result only", Verbosity.Data);
+            }
 
-			if (input.Mode == UserInput.WorkMode.Update)
-			{
-				if (String.IsNullOrEmpty(input.VersionMask))
-				{
-					Console.WriteLine("Version mask should be provided for 'update' mode, use '-v|-version' to set it");
-					return false;
-				}
-				var parts = input.VersionMask.Split('.').ToList();
-				if (parts.Count != 4)
-				{
-					Console.WriteLine("Version mask should in format 'a.b.c.d' where each letter stands for positive number or #");
-				}
-				var errors = new StringBuilder();
-				foreach (var part in parts)
-				{
-					if (part == "#")
-						continue;
-					uint number;
-					if (!uint.TryParse(part, out number))
-					{
-						errors.AppendFormat("Version part '{0}' cannot be parsed neither as positive number nor as #", part);
-						errors.AppendLine();
-					}
-				}
-				if (errors.Length > 0)
-					Console.WriteLine(errors.ToString());
+            if (String.IsNullOrWhiteSpace(input.Mode))
+            {
+                Console.WriteLine("Execution mode not set.");
+                return false;
+            }
 
-			}
-			return true;
-		}
-	}
+            pos = Array.IndexOf(_modes, input.Mode.ToLowerInvariant());
+            if (pos < 0)
+            {
+                Console.WriteLine("Unexpected execution mode. Allowed values are '{0}')", string.Join(", ", _modes));
+                return false;
+            }
+            _readMode = _modes[pos] == "read";
 
-	public static class Logger
-	{
-		[StringFormatMethod("template")]
-		private static void Data(string template, params object[] args)
-		{
-			Console.Write(template, args);
-		}
+            if (!_readMode)
+            {
+                if (String.IsNullOrWhiteSpace(input.VersionMask))
+                {
+                    Console.WriteLine("Version mask should be provided for 'read' execution mode");
+                    return false;
+                }
 
-		private static void Log(string template, params object[] args)
-		{
-			if ()
-			Console.Write(template, args);
-		}
-	}
+                var stringParts = input.VersionMask.Split('.').ToList();
+                if (stringParts.Count != 4)
+                {
+                    Console.WriteLine("Version mask should in format 'a.b.c.d' where each letter stands for positive number or #");
+                    return false;
+                }
 
-	public class Version
-	{
-		public string A { get; set; }
-		public string B { get; set; }
-		public string C { get; set; }
-		public string D { get; set; }
-	}
+                var errors = new StringBuilder();
+                var numberParts = new uint?[4];
+                for (int i = 0; i < stringParts.Count; i++)
+                {
+                    var part = stringParts[i];
+                    if (part == "#")
+                        continue;
+                    uint number;
+                    if (!uint.TryParse(part, out number))
+                    {
+                        errors.AppendFormat("Version part '{0}' cannot be parsed neither to positive number nor to #", part);
+                        errors.AppendLine();
+                    }
+                    else
+                    {
+                        numberParts[i] = number;
+                    }
+                }
+                if (errors.Length > 0)
+                {
+                    Console.WriteLine(errors.ToString());
+                    return false;
+                }
+                _versionParts = numberParts;
+            }
+            return true;
+        }
+    }
+
+    public class Version
+    {
+        public int? A { get; set; }
+        public int? B { get; set; }
+        public int? C { get; set; }
+        public int? D { get; set; }
+    }
+
+    public static class Lo
+    {
+        public static Verbosity Verbosity { get; set; }
+
+        [StringFormatMethod("template")]
+        public static void Data(string template, params object[] args)
+        {
+            if(CanWrite(Verbosity, Verbosity.Data))
+            Console.Write(template, args);
+        }
+
+        [StringFormatMethod("template")]
+        public static void Log(string template, params object[] args)
+        {
+            if (CanWrite(Verbosity, Verbosity.Normal))
+                Console.Write(template, args);
+        }
+
+        [StringFormatMethod("template")]
+        public static void Details(string template, params object[] args)
+        {
+            if (CanWrite(Verbosity, Verbosity.Detailed))
+                Console.Write(template, args);
+        }
+
+        private static bool CanWrite(Verbosity current, Verbosity given)
+        {
+            return ((int) given) <= ((int) current);
+        }
+    }
 }
