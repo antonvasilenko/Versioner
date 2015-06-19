b8d00a593be08ec6527c2c970fbbee87f57a7873
Anton Vasilenko <toxa07@gmail.com>
2015-06-19 08:42:09 +0300

    minor fix

--- Sandbox/update_touch_version.bat
+++ Sandbox/update_touch_version.bat
@@ -2,7 +2,7 @@
 
 for /f %%i in ('c:\work\binary\svn\Versioner\Sources\bin\Debug\Versioner.exe -f=c:\work\binary\svn\Versioner\Sandbox\Info.plist -m read -v data') do set OLD_VER=%%i
 
-c:\work\binary\svn\Versioner\Sources\bin\Debug\Versioner.exe -f=c:\work\binary\svn\Versioner\Sandbox\Info.plist -m update -v normal -ver "#.#.111.222"
+c:\work\binary\svn\Versioner\Sources\bin\Debug\Versioner.exe -f=c:\work\binary\svn\Versioner\Sandbox\Info.plist -m update -v normal -ver "#.#.333.444"
 
 for /f %%i in ('c:\work\binary\svn\Versioner\Sources\bin\Debug\Versioner.exe -f=c:\work\binary\svn\Versioner\Sandbox\Info.plist -m read -v data') do set NEW_VER=%%i
 echo version changed from %OLD_VER% to %NEW_VER%
