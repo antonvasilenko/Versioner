915d422c440b7450aeb200f032a481e9540a11c5
antonv <toxa07@gmail.com>
2015-11-24 09:35:03 +0200

    Added Csharp versioner logic to work with AssemblyVersion(Product version)

--- Sources/Versioner/Handlers/CsharpVersioner.cs
+++ Sources/Versioner/Handlers/CsharpVersioner.cs
@@ -1,32 +1,49 @@
 using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Versioner.Handlers
 {
     public class CsharpVersioner : IVersioner
     {
+        private string _fileContent;
+        private string _filePath;
+
         public bool CanHandle(string filePath)
         {
-            return false;
+            var fileExt = Path.GetExtension(filePath);
+            return ".cs".Equals(fileExt, StringComparison.InvariantCultureIgnoreCase);
         }
 
         public void Init(string filePath)
         {
-            throw new NotImplementedException();
+            _filePath = filePath;
+            _fileContent = File.ReadAllText(filePath);
         }
 
         public Version FetchVersion()
         {
-            throw new NotImplementedException();
+            var parseResult = Regex.Match(_fileContent, @"AssemblyVersion\s*\(\s*""([0-9\.\*]+)""\s*\)\s*\]");
+            if (parseResult.Success)
+                return new Version(parseResult.Groups[1].Value);
+            return null;
         }
 
-        public void UpdateVersion(Version version)
+        public void UpdateVersion(Version versionMask)
         {
-            throw new NotImplementedException();
+            var parseResult = Regex.Match(_fileContent, @"AssemblyVersion\s*\(\s*""([0-9\.\*]+)""\s*\)\s*\]");
+            if (parseResult.Success)
+            {
+                var oldVersion = new Version(parseResult.Groups[1].Value);
+                var newVersion = versionMask.ApplyTo(oldVersion);
+                _fileContent = _fileContent.Replace(parseResult.Groups[1].Value, newVersion.ToString());
+                Lo.Details("VersionName updated from {0} to {1}\n", oldVersion, newVersion);
+                File.WriteAllText(_filePath, _fileContent);
+            }
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
         }
     }
 }
